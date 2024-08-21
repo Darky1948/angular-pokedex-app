2a0b91f2d14a99fe57e9cd18f74c0b2e7c788d6b
Kristen Viguier <kristen.viguier@gmail.com>
2024-08-21 10:15:33 +0200

    Init angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name = 'Pikachu';
+  life = 21;
+
+  incrementLife(): void {
+    console.log("Increment")
+    this.life = this.life + 1;
+  }
+
+  decrementLife(): void {
+    console.log("Decrement")
+    this.life = this.life - 1;
+  }
+}
