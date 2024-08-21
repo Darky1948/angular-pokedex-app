a9df1ec897b3613967c2730b03cde269923a51d6
Kristen Viguier <kristen.viguier@gmail.com>
2024-08-21 10:47:29 +0200

    SIGNALS: discovery of signals in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Pikachu';
-  life = 21;
+  life = signal(21);
 
   incrementLife(): void {
     console.log("Increment")
-    this.life = this.life + 1;
+    this.life.update(n => n +1);
   }
 
   decrementLife(): void {
     console.log("Decrement")
-    this.life = this.life - 1;
+    this.life.update(n => n - 1);
   }
 }
