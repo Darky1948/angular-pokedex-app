9ef0feaf063641365ca6341318dffab52240c0b7
Kristen Viguier <kristen.viguier@gmail.com>
2024-09-21 18:22:54 +0200

    add a search field by using signals and computed based on input event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,11 @@ import { PokemonService } from './pokemon.service';
 })
 export class AppComponent {
   private readonly pokemonService = inject(PokemonService);
-
   pokemons: WritableSignal<PokemonList> = signal(this.pokemonService.getPokemonList());
+  readonly searchTerm = signal('');
+  readonly pokemonListFiltered = computed(() => {
+    return this.pokemons().filter((pokemon) => pokemon.name.toLowerCase().includes(this.searchTerm().trim().toLowerCase()));
+  });
 
   incrementLife(pokemon: Pokemon): void {
     console.log("Increment")
@@ -37,4 +40,5 @@ export class AppComponent {
       return "Moyen";
     }
   }
+
 }
