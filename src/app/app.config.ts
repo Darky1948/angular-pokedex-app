1e6170a763b119b1acd5501f70d3ce6c280fdcf4
Kristen Viguier <kristen.viguier@gmail.com>
2024-09-22 15:29:38 +0200

    404 not found and route handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // Defining routes array
 const routes: Routes = [
   { path: 'pokemons/:id', component: PokemonProfileComponent },
   { path: 'pokemons', component: PokemonListComponent },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' }, // Home default route that will redirect to /pokemons
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 export const appConfig: ApplicationConfig = {
