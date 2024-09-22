0e0d2b3ce51e9bb77f4a1aa31d06732d6cb1e457
Kristen Viguier <kristen.viguier@gmail.com>
2024-09-22 14:55:14 +0200

    Defining the content of comp pokemon profile. Retrieve the id from the route. Thanks to the pokemonservice get pokemon by id and display its title

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../pokemon.service';
 
 @Component({
   selector: 'app-pokemon-profile',
   standalone: true,
   imports: [],
   templateUrl: './pokemon-profile.component.html',
-  styles: ``
+  styles: ``,
+  providers: [PokemonService],
 })
 export class PokemonProfileComponent {
+  readonly route = inject(ActivatedRoute); // Service to access param of current route.
+  readonly pokemonService = inject(PokemonService); // PokemonService to access to pokemon data
+  readonly pokemonId = Number(this.route.snapshot.paramMap.get('id')); // Looking for pokemonId from the route.
+  readonly pokemon = signal(this.pokemonService.getPokemonById(this.pokemonId)).asReadonly(); // Retrieving the pokemon according to its id.
 
 }
