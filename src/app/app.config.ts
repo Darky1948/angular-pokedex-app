c44633393bff7604a71050b9be035739ad3475ed
Kristen Viguier <kristen.viguier@gmail.com>
2024-09-22 14:40:32 +0200

    Creating comp PokemonProfile and adding new route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
 
 // Defining routes array
 const routes: Routes = [
+  { path: 'pokemons/:id', component: PokemonProfileComponent },
   { path: 'pokemons', component: PokemonListComponent },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' }, // Home default route that will redirect to /pokemons
 ];
