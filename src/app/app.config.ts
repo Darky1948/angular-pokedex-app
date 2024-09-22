38094ada495ddc50de319e590fae11d310c0ca25
Kristen Viguier <kristen.viguier@gmail.com>
2024-09-22 14:36:57 +0200

    Configuring routes into the app. Now app.comp load pokemonlist

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+
+// Defining routes array
+const routes: Routes = [
+  { path: 'pokemons', component: PokemonListComponent },
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' }, // Home default route that will redirect to /pokemons
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), // Thanks to provideRouter will tell to angular what are the defined routes.
+  ],
 };
