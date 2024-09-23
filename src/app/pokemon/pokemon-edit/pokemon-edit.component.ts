0dd5c805b809c4bfc6e432994dc851188eab6b98
Kristen Viguier <kristen.viguier@gmail.com>
2024-09-23 06:57:52 +0200

    Using reactiveformsmodule to manage form with FormGroup FormControl and FormArray.

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -2,11 +2,12 @@ import { DatePipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-edit',
   standalone: true,
-  imports: [RouterLink, DatePipe],
+  imports: [RouterLink, DatePipe, ReactiveFormsModule],
   templateUrl: './pokemon-edit.component.html',
   styles: ``,
   providers: [PokemonService]
@@ -16,4 +17,11 @@ export class PokemonEditComponent {
   readonly pokemonService = inject(PokemonService); // PokemonService to access to pokemon data
   readonly pokemonId = Number(this.route.snapshot.paramMap.get('id')); // Looking for pokemonId from the route.
   readonly pokemon = signal(this.pokemonService.getPokemonById(this.pokemonId)).asReadonly(); // Retrieving the pokemon according to its id.
+
+  readonly form = new FormGroup({
+    name: new FormControl(this.pokemon().name),
+    life: new FormControl(this.pokemon().life),
+    damage: new FormControl(this.pokemon().damage),
+    types: new FormControl(this.pokemon().types),
+  });
 }
